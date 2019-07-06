ded8a562ec9a0606e909352f17aaefbcac4c65a5
Etienne Caron <etienne.caron@shopify.com>
2019-07-06 15:46:06 -0400

    Fan out

--- app/src/main/java/com/kanawish/upvote/common/FlowModelStore.kt
+++ app/src/main/java/com/kanawish/upvote/common/FlowModelStore.kt
@@ -1,9 +1,18 @@
 package com.kanawish.upvote.common
 
 import io.reactivex.Observable
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.FlowPreview
+import kotlinx.coroutines.ObsoleteCoroutinesApi
+import kotlinx.coroutines.cancelChildren
+import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.channels.ReceiveChannel
+import kotlinx.coroutines.channels.consumeEach
+import kotlinx.coroutines.channels.produce
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
 import kotlinx.coroutines.withTimeoutOrNull
 
@@ -24,13 +33,25 @@ open class FlowModelStore<S>(startingState: S) : ModelStore<S> {
 
 fun log(msg: String) = println("[${Thread.currentThread().name}] $msg")
 
-fun main() = runBlocking {
-    val result = withTimeoutOrNull(1300L) {
-        repeat(1000) { i ->
-            println("I'm sleeping $i ...")
-            delay(500L)
-        }
-        "Heyo."
+@ExperimentalCoroutinesApi
+fun CoroutineScope.produceNumbers() = produce<Int> {
+    var x = 1 // start from 1
+    while (true) {
+        send(x++) // produce next
+        delay(100) // wait 0.1s
     }
-    println("job was launched, result = $result")
+}
+
+fun CoroutineScope.launchProcessor(id: Int, channel: ReceiveChannel<Int>) = launch {
+    for (msg in channel) {
+        println("Processor #$id received $msg")
+    }
+}
+
+@ExperimentalCoroutinesApi
+fun main() = runBlocking<Unit> {
+    val producer = produceNumbers()
+    repeat(5) { launchProcessor(it, producer) }
+    delay(950)
+    producer.cancel() // cancel producer coroutine and thus kill them all
 }
