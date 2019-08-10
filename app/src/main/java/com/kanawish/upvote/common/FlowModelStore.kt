a2f29c11c46292500096bfb580945270652ce0ae
Etienne Caron <etienne.caron@shopify.com>
2019-08-10 15:56:26 -0400

    Playing around with `hu.akarnokd.kotlin.flow`, store not yet usable.

--- app/src/main/java/com/kanawish/upvote/common/FlowModelStore.kt
+++ app/src/main/java/com/kanawish/upvote/common/FlowModelStore.kt
@@ -1,55 +1,160 @@
 package com.kanawish.upvote.common
 
-import io.reactivex.Observable
+import hu.akarnokd.kotlin.flow.BehaviorSubject
+import hu.akarnokd.kotlin.flow.PublishSubject
+import hu.akarnokd.kotlin.flow.replay
 import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.FlowPreview
-import kotlinx.coroutines.ObsoleteCoroutinesApi
-import kotlinx.coroutines.cancelChildren
+import kotlinx.coroutines.MainScope
+import kotlinx.coroutines.cancel
 import kotlinx.coroutines.channels.Channel
-import kotlinx.coroutines.channels.ReceiveChannel
-import kotlinx.coroutines.channels.SendChannel
-import kotlinx.coroutines.channels.consumeEach
-import kotlinx.coroutines.channels.produce
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.flow.consumeAsFlow
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.onCompletion
+import kotlinx.coroutines.flow.scan
+import kotlinx.coroutines.joinAll
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.newSingleThreadContext
 import kotlinx.coroutines.runBlocking
-import kotlinx.coroutines.withTimeoutOrNull
+import kotlinx.coroutines.test.setMain
 
-@FlowPreview
-open class FlowModelStore<S>(startingState: S) : ModelStore<S> {
+/*
+ * Stuff to explore/fold in:
+ *
+ * - https://github.com/ZacSweers/CatchUp/tree/master/libraries/flowbinding
+ * - https://github.com/akarnokd/kotlin-flow-extensions
+ *
+ * Channels / Actors / moar Android specifics
+ * - https://github.com/Kotlin/kotlinx.coroutines/blob/master/ui/coroutines-guide-ui.md
+ * - https://geoffreymetais.github.io/code/coroutines/#scope
+ * - https://developer.android.com/kotlin/coroutines
+ * - https://github.com/Kotlin/kotlinx.coroutines/tree/master/ui/kotlinx-coroutines-android
+ */
 
-    var foo: Flow<S>? = null
+@FlowPreview @ExperimentalCoroutinesApi
+open class FlowModelStore<S>(startingState: S) : CoroutineScope by MainScope()  {
 
-    override fun process(intent: Intent<S>) {
-        TODO()
+    val i2 = Channel<Intent<S>>()
+        .also { channel ->
+            launch {
+                channel.consumeAsFlow()
+                    .scan(startingState) { oldState, intent ->
+                        intent.reduce(oldState)
+                    }
+                    .collect {
+                        println("store[$it]")
+                        s2.emit(it)
+                    }
+            }
+        }
+
+    // Multicaster
+    val s2 = BehaviorSubject<S>()
+
+    suspend fun foo() {
+        i2.send( intent { this } )
     }
 
-    override fun modelState(): Observable<S> {
-        TODO()
+    val intents = PublishSubject<Intent<S>>()
+
+    val store: Flow<S> = intents
+        .scan(startingState) { oldState, intent ->
+            intent.reduce(oldState)
+        }
+        .replay(1) { it }
+        .apply { launch { collect { println("store[$it]") } } }
+
+    /**
+     * Model will receive intents to be processed via this function.
+     *
+     * ModelState is immutable. Processed intents will work much like `copy()`
+     * and create a new (modified) modelState from an old one.
+     */
+    fun process(intent: Intent<S>)
+    {
+        launch {
+            intents.emit(intent)
+        }
     }
 
+    /**
+     * stream of changes to ModelState
+     *
+     * Every time a modelState is replaced by a new one, this observable will
+     * fire.
+     *
+     * This is what views will subscribe to.
+     */
+    fun modelState(): Flow<S> = store
+
+    fun destroy() {
+        launch {
+            intents.complete()
+        }
+        cancel()
+    }
 }
 
 fun log(msg: String) = println("[${Thread.currentThread().name}] $msg")
 
-/**
- * Fan-in
- */
-fun main() = runBlocking {
-    val channel = Channel<String>()
-    launch { sendString(channel, "foo", 200L) }
-    launch { sendString(channel, "BAR!", 500L) }
-    repeat(6) { // receive first six
-        println(channel.receive())
+@FlowPreview
+val flow: Flow<Int> = flow {
+    var i = 0
+    while (i < 6) {
+        delay(500)
+        println("Emit $i")
+        emit(i++)
     }
-    coroutineContext.cancelChildren() // cancel all children to let main finish
 }
 
-suspend fun sendString(channel: SendChannel<String>, s: String, time: Long) {
-    while (true) {
-        delay(time)
-        channel.send(s)
+@FlowPreview @ExperimentalCoroutinesApi
+fun main() = runBlocking<Unit> {
+    val mainThreadSurrogate = newSingleThreadContext("UI thread")
+    Dispatchers.setMain(mainThreadSurrogate)
+
+    val storeCounter = FlowModelStore(0)
+
+//    launch { flow.collect { println("A: got $it") } }
+//    launch { flow.collect { println("B: got $it") } }
+
+    launch {
+        // Outputs into the storeCounter
+        flow
+            .onCompletion {
+                println("completion -> intents.complete()")
+                storeCounter.intents.complete()
+            }.collect { counter ->
+                println("intent{this+$counter}")
+                storeCounter.process(intent { this+counter })
+            }
+
     }
+
+    delay(2000)
+    println("Launch j1")
+    launch {
+        storeCounter.modelState().collect {
+            println("j1[$it]")
+        }
+    }
+    delay(1000)
+    println("Launch j2")
+    launch {
+        storeCounter.modelState().collect {
+            println("j2[$it]")
+        }
+    }
+
+    delay(100)
+
+    println("joinAll()")
+    joinAll()
+
+    println("end of main()")
+    mainThreadSurrogate.close()
 }
--- docs/NOTES.md
+++ docs/NOTES.md
@@ -6,4 +6,4 @@
 - [Kotlin Experimental markers](https://kotlinlang.org/docs/reference/experimental.html)
 - https://spring.io/blog/2019/04/12/going-reactive-with-spring-coroutines-and-kotlin-flow
 - https://medium.com/@elizarov
-- 
+# - 
